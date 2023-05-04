32719dac509bf0dfe7a55c60475df16182e9f1f1
jimkk159 <jimkk159@gmail.com>
2023-05-04 10:03:53 +0800

    fix: change the email verify url to env

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -107,9 +107,9 @@ export const signup = catchAsync(async (req, res, next) => {
 
   // 7) Send an verification email
   const email = new Email(newUser.email, newUser.firstName);
-  const verifyUrl = `${req.protocol}://${req.hostname}:${process.env.PORT}/api/v1/auths/verify_email?token=${token}`;
+  const verifyUrl =
+    process.env.SERVER_URL + `/api/v1/auths/verify_email?token=${token}`;
   await email.sendWelcome(verifyUrl).catch((err) => {
-    console.log(err)
     throw errorTable.sendEmailError();
   });
 
