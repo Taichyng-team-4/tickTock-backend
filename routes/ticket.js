b7d47af3edd64e54f5196aab036f20e0e880e5ec
jimkk159 <jimkk159@gmail.com>
2023-05-27 22:36:16 +0800

    feat: ticket create, update, delete

--- routes/ticket.js
+++ routes/ticket.js
@@ -1,17 +1,32 @@
 import express from "express";
+import Ticket from "../models/ticket.js";
+import { check } from "express-validator";
+import * as factory from "../controllers/factory.js";
 
 import * as ticketController from "../controllers/ticketControllers.js";
+import * as shareControllers from "../controllers/shareControllers.js";
+import * as authControllers from "../controllers/authControllers.js";
 
 const router = express.Router();
 
+router.use(authControllers.authToken);
+
 router
   .route("/")
-  .get(ticketController.getAll)
-  .post(ticketController.createOne);
+  .get(factory.getAll(Ticket))
+  .post(
+    [
+      check("activityId").notEmpty(),
+      check("ticketTypeId").notEmpty(),
+    ],
+    shareControllers.validation,
+    ticketController.createOne
+  );
+
 router
-  .route("/:ticketId")
-  .get(ticketController.getOne)
-  .patch(ticketController.updateOne)
-  .delete(ticketController.deleteOne);
+  .route("/:id")
+  .get(ticketController.checkOwner, factory.getOne(Ticket))
+  .patch(factory.updateOne(Ticket))
+  .delete(ticketController.checkOwner, factory.deleteOne(Ticket));
 
 export default router;
